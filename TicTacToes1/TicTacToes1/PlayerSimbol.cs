d1b3e7693b18241ba2f54deae81c69328ec5eec1
Gundars Eimanis <gundarens@gmail.com>
2022-05-17 21:09:03 +0300

    +

---
+++ TicTacToes1/TicTacToes1/PlayerSimbol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToes1
+{
+    public class PlayerSimbol
+    {
+        public static string firstSimbol = "X";
+        public static string secondSimbol = "O";
+    }
+}
