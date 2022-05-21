dad5269ab694e0b09575fccd66328a649dc0e96a
AgyLV <70198664+AgyLV@users.noreply.github.com>
2022-05-21 20:15:34 +0300

    Update CheckWinner.cs
    manualas izmainas

--- TicTacToes1/TicTacToes1/CheckWinner.cs
+++ TicTacToes1/TicTacToes1/CheckWinner.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,41 +10,15 @@ namespace TicTacToes1
     {
         public static int Winner(string[] gameBoard)
         {
-            if (gameBoard[1] == gameBoard[2] && gameBoard[2] == gameBoard[3])
+            if ((gameBoard[1] == gameBoard[2] && gameBoard[2] == gameBoard[3]) || (gameBoard[4] == gameBoard[5] && gameBoard[5] == gameBoard[6]) || (gameBoard[7] == gameBoard[8] && gameBoard[8] == gameBoard[9]) || (gameBoard[7] == gameBoard[8] && gameBoard[8] == gameBoard[9]) || (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7]) || (gameBoard[2] == gameBoard[5] && gameBoard[5] == gameBoard[8]) || (gameBoard[3] == gameBoard[6] && gameBoard[6] == gameBoard[9]) || (gameBoard[1] == gameBoard[5] && gameBoard[5] == gameBoard[9]) || (gameBoard[3] == gameBoard[5] && gameBoard[5] == gameBoard[7]))
             {
-                return 1;
+               return 1;
             }
-            else if (gameBoard[4] == gameBoard[5] && gameBoard[5] == gameBoard[6])
-            {
-                return 1;
-            }
-            else if (gameBoard[7] == gameBoard[8] && gameBoard[8] == gameBoard[9])
-            {
-                return 1;
-            }
-            else if (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7])
-            {
-                return 1;
-            }
-            else if (gameBoard[2] == gameBoard[5] && gameBoard[5] == gameBoard[8])
-            {
-                return 1;
-            }
-            else if (gameBoard[3] == gameBoard[6] && gameBoard[6] == gameBoard[9])
-            {
-                return 1;
-            }
-            else if (gameBoard[1] == gameBoard[5] && gameBoard[5] == gameBoard[9])
-            {
-                return 1;
-            }
-            else if (gameBoard[3] == gameBoard[5] && gameBoard[5] == gameBoard[7])
-            {
-                return 1;
-            }
+            
+                            
             else if (gameBoard[1] != "1" && gameBoard[2] != "2" && gameBoard[3] != "3" && gameBoard[4] != "4" && gameBoard[5] != "5" && gameBoard[6] != "6" && gameBoard[7] != "7" && gameBoard[8] != "8" && gameBoard[9] != "9")
             {
-                return 2;
+               return 2;
             }
             return 0;
 
