e0a719a5a65f2b2ed353c06ccd2e794c588c5004
agnese.veze <agnese.veze@gmail.com>
2022-05-21 20:10:27 +0300

    Agy

--- TicTacToes1/TicTacToes1/CheckWinner.cs
+++ TicTacToes1/TicTacToes1/CheckWinner.cs
@@ -10,38 +10,12 @@ namespace TicTacToes1
     {
         public static int Winner(string[] gameBoard)
         {
-            if (gameBoard[1] == gameBoard[2] && gameBoard[2] == gameBoard[3])
+            if ((gameBoard[1] == gameBoard[2] && gameBoard[2] == gameBoard[3]) || (gameBoard[4] == gameBoard[5] && gameBoard[5] == gameBoard[6]) || (gameBoard[7] == gameBoard[8] && gameBoard[8] == gameBoard[9]) || (gameBoard[7] == gameBoard[8] && gameBoard[8] == gameBoard[9]) || (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7]) || (gameBoard[2] == gameBoard[5] && gameBoard[5] == gameBoard[8]) || (gameBoard[3] == gameBoard[6] && gameBoard[6] == gameBoard[9]) || (gameBoard[1] == gameBoard[5] && gameBoard[5] == gameBoard[9]) || (gameBoard[3] == gameBoard[5] && gameBoard[5] == gameBoard[7]))
             {
                 return 1;
             }
-            else if (gameBoard[4] == gameBoard[5] && gameBoard[5] == gameBoard[6])
-            {
-                return 1;
-            }
-            else if (gameBoard[7] == gameBoard[8] && gameBoard[8] == gameBoard[9])
-            {
-                return 1;
-            }
-            else if (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7])
-            {
-                return 1;
-            }
-            else if (gameBoard[2] == gameBoard[5] && gameBoard[5] == gameBoard[8])
-            {
-                return 1;
-            }
-            else if (gameBoard[3] == gameBoard[6] && gameBoard[6] == gameBoard[9])
-            {
-                return 1;
-            }
-            else if (gameBoard[1] == gameBoard[5] && gameBoard[5] == gameBoard[9])
-            {
-                return 1;
-            }
-            else if (gameBoard[3] == gameBoard[5] && gameBoard[5] == gameBoard[7])
-            {
-                return 1;
-            }
+
+
             else if (gameBoard[1] != "1" && gameBoard[2] != "2" && gameBoard[3] != "3" && gameBoard[4] != "4" && gameBoard[5] != "5" && gameBoard[6] != "6" && gameBoard[7] != "7" && gameBoard[8] != "8" && gameBoard[9] != "9")
             {
                 return 2;
