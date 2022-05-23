f7ef44fd736950203682d8c28a5ddeab1813875f
agnese.veze <agnese.veze@gmail.com>
2022-05-23 23:23:08 +0300

    aizliedz ievadīt aizņemtu lauku

--- TicTacToes1/TicTacToes1/CallingGameBoard.cs
+++ TicTacToes1/TicTacToes1/CallingGameBoard.cs
@@ -8,6 +8,15 @@ namespace TicTacToes1
 {
     public class CallingGameBoard
     {
+        public int Field1 { get; private set; }
+        public int Field2 { get; private set; }
+        public int Field3 { get; private set; }
+        public int Field4 { get; private set; }
+        public int Field5 { get; private set; }
+        public int Field6 { get; private set; }
+        public int Field7 { get; private set; }
+        public int Field8 { get; private set; }
+        public int Field9 { get; private set; }
         public static void GameBoard(string[] gameBoard)
         {
             Console.WriteLine(@"
@@ -36,6 +45,83 @@ namespace TicTacToes1
             Console.WriteLine("         |         |         ");
             Console.WriteLine($"    {gameBoard[7]}    |    {gameBoard[8]}    |    {gameBoard[9]}    ");
             Console.WriteLine("         |         |         ");
+        }
+
+        public void IrAiznemts(string[] gameBoard, string playerSimbol, int value)
+        {
+            
+            {
+                switch (value)
+                {
+                    case 1:
+                        if (Field1 != 1) ;
+                        {
+                            return;
+                        }
+                        Field1 = value;
+                        break;
+                    case 2:
+                        if (Field2 != 2) ;
+                        {
+                            return;
+                        }
+                        Field2 = value;
+                        break;
+                    case 3:
+                        if (Field3 != 3) ;
+                        {
+                            return;
+                        }
+                        Field3 = value;
+                        break;
+                    case 4:
+                        if (Field4 != 4) ;
+                        {
+                            return;
+                        }
+                        Field4 = value;
+                        break;
+                    case 5:
+                        if (Field5 != 5) ;
+                        {
+                            return;
+                        }
+                        Field5 = value;
+                        break; ;
+                    case 6:
+                        if (Field6 != 6) ;
+                        {
+                            return;
+                        }
+                        Field6 = value;
+                        break;
+                    case 7:
+                        if (Field7 != 7) ;
+                        {
+                            return;
+                        }
+                        Field7 = value; 
+                        break;
+                    case 8:
+                        if (Field8 != 8) ;
+                        {
+                            return;
+                        }
+                        Field8 = value;
+                        break;
+                    case 9:
+                        if (Field9 != 9) ;
+                        {
+                            return;
+                        }
+                        Field9 = value;
+                        break;
+                    default:
+                        break;
+
+
+                }
+            }
         }
     }
 }
--- TicTacToes1/TicTacToes1/GAMEBOARD.cs
+++ TicTacToes1/TicTacToes1/GAMEBOARD.cs
@@ -69,36 +69,38 @@ namespace TicTacToes1
 
 
 
-            for (int i = 2; i < gameBoard.Length - 1;)
+            for (int i = 0; i < gameBoard.Length - 1;)
             {
                 int uzvaretajs = CheckWinner.Winner(gameBoard);
 
 
 
-                if (uzvaretajs == 1)
-                {
-                    Console.WriteLine($"Winner!");
+                //if (uzvaretajs == 1)
+                //{
+                //    Console.WriteLine($"Winner!");
 
 
-                    if (gameBoard[i] == player1Simbol)
-                    {
-                        Console.WriteLine($"{player1} wins!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{player2} wins!");
-                    }
+                //    if (gameBoard[i] == player1Simbol)
+                //    {
+                //        Console.WriteLine($"{player1} wins!");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine($"{player2} wins!");
+                //    }
 
 
 
 
-                    break;
-                }
+                //    break;
+                //}
+
                 if (i % 2 == 0)
                 {
 
                     Console.WriteLine($"{player1} choose your field!");
-                    string choise = Console.ReadLine();
+                    //string choise = Console.ReadLine();
+                    int choise = int.Parse(Console.ReadLine());
 
                     // while (choise != "1" || choise != "2" || choise != "3" || choise != "4" || choise != "5" || choise != "6" || choise != "7" || choise != "8" || choise != "9")
                     //{
@@ -108,19 +110,33 @@ namespace TicTacToes1
 
                     i++;
 
-                    int choise1 = int.Parse(choise);
-                    gameBoard[choise1] = player1Simbol;
+                    //int choise1 = int.Parse(choise);
+                    gameBoard[choise] = player1Simbol;
 
-                    CheckWinner.Winner(gameBoard);
+                    uzvaretajs = CheckWinner.Winner(gameBoard); // kombinācijas vērtību pārnesam uz mainīgo, proti, vai ir 3 simboli pēc kārtas
                     Console.Clear();
                     CallingGameBoard.GameBoard(gameBoard);
 
+                    if (uzvaretajs == 1)  //uzreiz pēc spēlētāja gājiena pārbauda vai ir uzvarējis
+                    {
+                        Console.WriteLine($"Winner: {player1} 1!");
+
+                        break;
+                    }
+
+                    if (i == 9)
+                    // kad būs veikti 9 gājieni, bet vēl nebūs noteikt uzvarētājs, tad rezultāts būs neizšķirts.
+                    // Pirmajam spēlētājam sanāk par vienu gājienu vairāk, tāpēc šī pārbaude notiek pēc 1. spēlētāja veiktā gājiena.
+                    {
+                        Console.WriteLine($"Neizskirts starp spēlētāju {player1} un {player2}!");
+                        break;
+                    }
 
                 }
                 if (i % 2 != 0)
                 {
                     Console.WriteLine($"{player2} choose your field!");
-                    string choise = Console.ReadLine();
+                    int choise = int.Parse(Console.ReadLine());
                     //while (choise != "1" || choise != "2" || choise != "3" || choise != "4" || choise != "5" || choise != "6" || choise != "7" || choise != "8" || choise != "9")
                     // {
                     //    Console.WriteLine($"{player2} choose your field!");
@@ -128,37 +144,52 @@ namespace TicTacToes1
                     // }
 
                     i++;
+                    bool IrAiznemts = false;
+                if (!IrAiznemts)
+                    {
+                        Console.WriteLine("Field is already taken!");
+                        Console.Write("Choose other field");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
 
 
+                    //int choise1 = int.Parse(choise);
 
-                    int choise1 = int.Parse(choise);
-
-                    gameBoard[choise1] = player2Simbol;
-                    CheckWinner.Winner(gameBoard);
+                    gameBoard[choise] = player2Simbol;
+                    uzvaretajs = CheckWinner.Winner(gameBoard);
                     Console.Clear();
                     CallingGameBoard.GameBoard(gameBoard);
 
-
-                    static bool Aiznemts(int choise, int choise1, string PlayerSimbol, string[] gameBoard, string player1Simbol, string player2Simbol)
+                    if (uzvaretajs == 1)  //uzreiz pēc spēlētāja gājiena pārbauda vai ir uzvarējis
                     {
-                        for (int i = 2; i < gameBoard.Length - 1;)
-                        {
-                            if (gameBoard[choise] == player2Simbol || gameBoard[choise1] == player1Simbol) // pārbauda vai lukums jau nav aizņemts
-                            {
-                                Console.WriteLine("Field is already taken!");
-                                Console.Write("Choose other field");
-                                Console.ReadLine();
-                                Console.Clear();
-                                return true;
-                            }
-                        }
-                        gameBoard[choise] = PlayerSimbol;
-                        gameBoard[choise1] = PlayerSimbol;
+                        Console.WriteLine($"Winner: {player1} 2!");
 
-                        return false;
-
-
+                        break;
                     }
+
+                    
+
+                        //static bool Aiznemts(int choise, int choise1, string PlayerSimbol, string[] gameBoard, string player1Simbol, string player2Simbol)
+                        //{
+                        //    for (int i = 2; i < gameBoard.Length - 1;)
+                        //    {
+                        //        if (gameBoard[choise] == player2Simbol || gameBoard[choise1] == player1Simbol) // pārbauda vai lukums jau nav aizņemts
+                        //        {
+                        //            Console.WriteLine("Field is already taken!");
+                        //            Console.Write("Choose other field");
+                        //            Console.ReadLine();
+                        //            Console.Clear();
+                        //            return true;
+                        //        }
+                        //    }
+                        //    gameBoard[choise] = PlayerSimbol;
+                        //    gameBoard[choise1] = PlayerSimbol;
+
+                        //    return false;
+
+
+                        //}
 
 
 
@@ -172,29 +203,29 @@ namespace TicTacToes1
 
 
 
-                //string[] gameBoard = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+                    //string[] gameBoard = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-                //gameBoard[1] = "XX"; // NEMAINIJU TESTA PĒC ŠO LĪNIJU JĀDZĒŠ ARA
+                    //gameBoard[1] = "XX"; // NEMAINIJU TESTA PĒC ŠO LĪNIJU JĀDZĒŠ ARA
 
-                //Console.WriteLine("         |         |         ");
-                //Console.WriteLine($"    {gameBoard[1]}    |    {gameBoard[2]}    |    {gameBoard[3]}    ");
-                //Console.WriteLine("         |         |         ");
-                //Console.WriteLine("-----------------------------");
-                //Console.WriteLine("         |         |         ");
-                //Console.WriteLine($"    {gameBoard[4]}    |    {gameBoard[5]}    |    {gameBoard[6]}    ");
-                //Console.WriteLine("         |         |         ");
-                //Console.WriteLine("-----------------------------");
-                //Console.WriteLine("         |         |         ");
-                //Console.WriteLine($"    {gameBoard[7]}    |    {gameBoard[8]}    |    {gameBoard[9]}    ");
-                //Console.WriteLine("         |         |         ");
-
-
+                    //Console.WriteLine("         |         |         ");
+                    //Console.WriteLine($"    {gameBoard[1]}    |    {gameBoard[2]}    |    {gameBoard[3]}    ");
+                    //Console.WriteLine("         |         |         ");
+                    //Console.WriteLine("-----------------------------");
+                    //Console.WriteLine("         |         |         ");
+                    //Console.WriteLine($"    {gameBoard[4]}    |    {gameBoard[5]}    |    {gameBoard[6]}    ");
+                    //Console.WriteLine("         |         |         ");
+                    //Console.WriteLine("-----------------------------");
+                    //Console.WriteLine("         |         |         ");
+                    //Console.WriteLine($"    {gameBoard[7]}    |    {gameBoard[8]}    |    {gameBoard[9]}    ");
+                    //Console.WriteLine("         |         |         ");
 
 
 
 
 
+
+
+                }
             }
         }
     }
-}
